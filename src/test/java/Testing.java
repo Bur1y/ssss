7fb99513c8e8918932799e3dc3565c34b49da345
kotomkin52@mail.ru <Djk529U56jPL34>
2022-06-07 15:57:47 +0300

    С тебя пиво

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,12 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.time.LocalTime;
 
 public class Testing {
     // Константы
-    static final LocalTime startTimes[] = {LocalTime.parse("10:00:00"), LocalTime.parse("11:00:00"), LocalTime.parse("15:00:00"), LocalTime.parse("15:30:00"), LocalTime.parse("16:50:00")};
+    static final LocalTime STARTTIMES[] = {LocalTime.parse("10:00:00"), LocalTime.parse("11:00:00"), LocalTime.parse("15:00:00"), LocalTime.parse("15:30:00"), LocalTime.parse("16:50:00")};
     static final int[] DURATIONS = {60, 30, 10, 10, 40};
-    static final LocalTime beginWorkingTime = LocalTime.parse("08:00");
-    static final LocalTime  ENDWRKINGTIME = LocalTime.parse("18:00");
+    static final LocalTime BEGINWOORKINGTIME = LocalTime.parse("08:00");
+    static final LocalTime  ENDWORKINGTIME = LocalTime.parse("18:00");
     static final int consultationTime = 30;
 
+
+    @Test
+    public static void isString(){
+        String s = null;
+        Assertions.assertSame(Calculations.availablePeriods(STARTTIMES,DURATIONS,BEGINWOORKINGTIME,ENDWORKINGTIME,consultationTime), s);
+    }
+
+    @Test
+    public static void notNull(){
+        Assertions.assertNotNull(Calculations.availablePeriods(STARTTIMES,DURATIONS,BEGINWOORKINGTIME,ENDWORKINGTIME,consultationTime));
+    }
 
 }
